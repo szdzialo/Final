9283bdce7231ca77d10e1a87754c02bc48540367
Sam <szdzialo@quinnipiac.edu>
2015-12-04 20:05:42 +0000

    Add question create view

--- core/models.py
+++ core/models.py
@@ -1,5 +1,14 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Question(models.Model):
+        title = models.CharField(max_length=500)
+        description = models.TextField(null=True, blank=True)
+        created_at = models.DateTimeField(auto_now_add=True)
+        user = models.ForeignKey(User)
+        
+        def __unicode__(self):
+                return self.title
